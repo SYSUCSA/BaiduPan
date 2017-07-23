94c5bf0656ae2e6a318a95f08860dcfc839a0f8f
ssst0n3 <ssst0n3@gmail.com>
2017-07-23 15:09:23 +0800

    authorize

--- BaiduPan.py
+++ BaiduPan.py
@@ -1,18 +1,18 @@
 # coding=utf-8
 import requests
-
-URL_PASSORT = "https://passport.baidu.com/v2/api/?login"
+from config import AUTH_URL
 
 
 class BaiduPan:
-    def __init__(self, username, password):
-        self._username = username
-        self._password = password
+    def __init__(self, api_key):
+        self._api_key = api_key
+        self._auth_url = AUTH_URL.replace("client_id=", "client_id="+self._api_key)
         self._session = requests.Session()
+        self._authorize()
 
-    def _login(self):
-        login_data = {
-            "username": self._username,
-            "password": self._password,
-        }
-        r = self._session.post(URL_PASSORT, data=login_data)
+    def _authorize(self):
+        msg = 'Please visit:\n{authorize_url}\nAnd authorize this app\n' \
+              'Paste the Authorization Code here within 10 minutes.'.format(authorize_url=self._auth_url)
+        print msg
+        auth_code = raw_input('Input auth_code > ')
+        print auth_code
---
+++ config.py
@@ -0,0 +1,1 @@
+AUTH_URL = "https://openapi.baidu.com/oauth/2.0/authorize?scope=netdisk&redirect_uri=oob&response_type=code&client_id="
