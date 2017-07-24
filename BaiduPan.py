d211ff3d5a66f18e671ce4232c3e45b19fa250c4
ssst0n3 <ssst0n3@gmail.com>
2017-07-24 15:21:07 +0800

    优化配置信息

--- BaiduPan.py
+++ BaiduPan.py
@@ -2,9 +2,9 @@
 import json
 import requests
 from lib.dict2url import dict2url, url2dict
-from config import URL_AUTH, PARAM_AUTH, RESPONSE_TYPE_TOKEN, RESPONSE_TYPE_AUTH_CODE, URL_TOKEN, PARAM_TOKEN
+from config import URL_AUTHORIZE, PARAM_AUTH, RESPONSE_TYPE_TOKEN, RESPONSE_TYPE_AUTH_CODE, URL_TOKEN
 from config import TOKEN_TYPE_WEB_SERVER_FLOW, TOKEN_TYPE_USER_AGENT_FLOW
-from config import URL_QUOTA, PARAM_QUOTA
+from config import URL_PCS_REST, PARAM_QUOTA
 
 
 class AuthorizeError(Exception):
@@ -51,20 +51,23 @@ class BaiduPanBase:
             self._get_access_token_user_agent_flow()
 
     def _get_access_token_web_server_flow(self):
-        param_token = PARAM_TOKEN
-        param_token["code"] = self._auth_code
-        param_token["client_id"] = self._api_key
-        param_token["client_secret"] = self._secret_key
-        r = self._session.get(URL_TOKEN, params=PARAM_TOKEN)
+        param_token = {
+            "grant_type": "authorization_code",
+            "code": self._auth_code,
+            "client_id": self._api_key,
+            "client_secret": self._secret_key,
+            "redirect_uri": "oob",
+        }
+        r = self._session.get(URL_TOKEN, params=param_token)
         data = json.loads(r.text)
         print r.request.url
         self._access_token = data["access_token"]
 
     def _authorize(self):
-        auth_param = PARAM_AUTH
-        auth_param["client_id"] = self._api_key
-        auth_param["response_type"] = RESPONSE_TYPE_AUTH_CODE
-        auth_url = URL_AUTH + "?" + dict2url(auth_param)
+        param_auth = PARAM_AUTH
+        param_auth["client_id"] = self._api_key
+        param_auth["response_type"] = RESPONSE_TYPE_AUTH_CODE
+        auth_url = URL_AUTHORIZE + "?" + dict2url(param_auth)
         msg = 'Please visit:\n{authorize_url}\nAnd authorize this app\n' \
               'Paste the Authorization Code here within 10 minutes.'.format(authorize_url=auth_url)
         print msg
@@ -72,10 +75,10 @@ class BaiduPanBase:
         self._auth_code = auth_code
 
     def _get_access_token_user_agent_flow(self):
-        auth_param = PARAM_AUTH
-        auth_param["client_id"] = self._api_key
-        auth_param["response_type"] = RESPONSE_TYPE_TOKEN
-        auth_url = URL_AUTH + "?" + dict2url(auth_param)
+        param_auth = PARAM_AUTH
+        param_auth["client_id"] = self._api_key
+        param_auth["response_type"] = RESPONSE_TYPE_TOKEN
+        auth_url = URL_AUTHORIZE + "?" + dict2url(param_auth)
         msg = 'Please visit:\n{authorize_url}\nAnd authorize this app\n' \
               'Paste the url or params here within 10 minutes.'.format(authorize_url=auth_url)
         print msg
@@ -90,18 +93,18 @@ class BaiduPan(BaiduPanBase):
         self._appname = app_name
 
     def quota(self):
-        param_quota = PARAM_QUOTA
-        param_quota["access_token"] = self._access_token
-        r = self._session.get(URL_QUOTA, params=param_quota)
-        print r.request.url
+        param_quota = {
+            "method": "info",
+            "access_token": self._access_token,
+        }
+        r = self._session.get(URL_PCS_REST.format(act="quota"), params=param_quota)
         print r.text
 
     def file_list(self):
-        url = "https://pcs.baidu.com/rest/2.0/pcs/file"
         param = {
             "method": "list",
             "access_token": self._access_token,
             "path": "/apps/" + self._appname
         }
-        r = self._session.get(url, params=param)
+        r = self._session.get(URL_PCS_REST.format(act="file"), params=param)
         print r.text
--- config.py
+++ config.py
@@ -7,27 +7,21 @@ TOKEN_TYPE_USER_AGENT_FLOW = "TOKEN_TYPE_USER_AGENT_FLOW"
 
 RESPONSE_TYPE_TOKEN = "token"
 RESPONSE_TYPE_AUTH_CODE = "code"
-URL_AUTH = "https://openapi.baidu.com/oauth/2.0/authorize"
+
+URL_OPENAPI = "https://openapi.baidu.com"
+URL_OAUTH = URL_OPENAPI + "/oauth/2.0"
+URL_AUTHORIZE = URL_OAUTH + "/authorize"
+URL_TOKEN = URL_OAUTH + "/token"
+
+URL_PCS = "https://pcs.baidu.com"
+URL_PCS_REST = URL_PCS + "/rest/2.0/pcs/{act}"
+
 PARAM_AUTH = {
     "scope": "netdisk",
     "redirect_uri": "oob",
     "response_type": "",
     "client_id": "",
 }
-URL_TOKEN = "https://openapi.baidu.com/oauth/2.0/token"
-PARAM_TOKEN = {
-    "grant_type": "authorization_code",
-    "code": "",
-    "client_id": "",
-    "client_secret": "",
-    "redirect_uri": "oob",
-}
-URL_QUOTA = "https://pcs.baidu.com/rest/2.0/pcs/quota"
-PARAM_QUOTA = {
-    "method": "info",
-    "access_token": "",
-}
-
 
 if __name__ == '__main__':
     from lib.dict2url import dict2url
